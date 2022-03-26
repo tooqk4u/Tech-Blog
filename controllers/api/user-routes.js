2132e9bc107d135ba4a9a57774842ca0e87e39fe
tooqk4u <tooqk4u@protonmail.com>
2022-03-25 23:46:01 -0400

    set up route index and user routes get and post

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,52 @@
+const router = require('express').Router();
+
+const { User } = require('../../models');
+
+
+
+router.get('/', (req, res) => {
+    User.findAll({
+        attributes: { exclude: ['password'] },
+    })
+    .then((dbUserData) => res.json(dbUserData))
+    .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+
+router.get('/:id', (req, res) => {
+    User.findOne({
+        attributes: { exclude: ['password'] },
+        where: {
+            id: req.params.id,
+        },
+        
+    })
+    .then((dbUserData) => res.json(dbUserData))
+    .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+})
+
+
+router.post('/', (req, res) => {
+    User.create({
+        username: req.body.username,
+        password: req.body.password,
+    })
+    .then(dbUserData => {
+        
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+
+
+module.exports = router; 
