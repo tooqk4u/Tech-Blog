8c7e5742f51baa307a41c4581da030ead72b9a0c
tooqk4u <tooqk4u@protonmail.com>
2022-03-26 19:56:34 -0400

    added password verfication

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -68,4 +68,30 @@ router.post("/", (req, res) => {
     });
 });
 
+router.post("/login", (req, res) => {
+  User.findOne({
+    where: {
+      username: req.body.username,
+    },
+  })
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        res.status(400).json({ message: "No user found with that username." });
+        return;
+      }
+
+      // user verification
+      const validPassword = dbUserData.checkPassword(req.body.password);
+      if (!validPassword) {
+        res.status(400).json({ message: "Password does not match with username." });
+        return;
+      }
+      res.json({ user: dbUserData, message: "You are now logged in!" });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const routes = require('./controllers');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
+const path = require('path');
 
 
 const app = express();
