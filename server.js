40380c05e1d1aee002d944eb9b3abdc00ec8775b
tooqk4u <tooqk4u@protonmail.com>
2022-03-26 18:12:59 -0400

    added homepage and main handlebars/home-routes & update server.js

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const sequelize = require("./config/connection");
 const express = require('express');
 const routes = require('./controllers');
-
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
 
 const app = express();
 
+
+
 const PORT = process.env.PORT || 3001
-// handlebars engine
-// handlebars set 
+
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // use session
 app.use(express.json());
