f6f467b4eb4717cc3280004ea3f247dd2c356bbf
tooqk4u <tooqk4u@protonmail.com>
2022-03-26 20:21:24 -0400

    updated homepage, added helpers and styling

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,7 +3,16 @@ const sequelize = require("../config/connection");
 const { Post, User, Comment } = require("../models");
 
 router.get("/", (req, res) => {
-  res.render("homepage");
-    });
-  
+  res.render("homepage", {
+    id: 1,
+    post_title: "This is a new post!",
+    post_body:
+      "This new post is about Javascript. Javascript can be considered the function portion of a website.",
+    created_at: new Date(),
+    comments: 30,
+    user: {
+      username: "test_user",
+    },
+  });
+});
 module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const sequelize = require("./config/connection");
 const express = require('express');
 const routes = require('./controllers');
+const helpers = require('./utils/helpers.js')
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 const path = require('path');
+
 
 
 const app = express();
@@ -20,6 +22,8 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // static
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
