bf081dbf19b38fa7eb0ff878d7d7e7310fba5d9e
tooqk4u <tooqk4u@protonmail.com>
2022-04-03 15:34:15 -0700

    edited main.js

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use(session(sess));
 
 //middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 
 // static
 app.use(express.static(path.join(__dirname, "public")));
